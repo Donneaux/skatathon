5e832354f7e73e0cee48c1c620828596865826ca
Josh Donnoe <josh.donnoe@gmail.com>
2019-08-11 13:39:17 -0400

    register page lists registered skaters

--- register.php
+++ register.php
@@ -1,1 +1,19 @@
+<?php
+	require_once('db_connect.php');
+
+	?>
+		<ul>
+		<?php
+			foreach (mysqli_query($link, "SELECT name FROM `skater`") as $row) {
+				?>
+					<li>
+						<?= $row['name'] ?>
+					</li>
+				<?php
+			}
+		?>
+		</ul>
+	<?php
+?>
+<br />
 this will evenutally be a page to register a skater
