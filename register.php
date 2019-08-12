6997fd26c4028e95faad950f1820628752f3b2c5
Josh Donnoe <josh.donnoe@gmail.com>
2019-08-11 23:19:25 -0400

    now the list is prefaced with the name of the event

--- register.php
+++ register.php
@@ -1,18 +1,18 @@
 <?php
 	require_once('db_connect.php');
 	require_once('event.php');
-	?>
-		<ul>
-		<?php
-			foreach (mysqli_query($link, "SELECT skater_num, name as skater_name FROM `registration` join skater on skater.num = skater_num where event_num='".$event."'") as $row) {
-				?>
-					<li>
-						<?= $row['skater_name'] ?>
-					</li>
-				<?php
-			}
-
-		?>
-		</ul>
+?>	
+<?= mysqli_query($link, "SELECT name as event_name from event where num=".$event) -> fetch_assoc()['event_name'] ?>
+<ul>
 	<?php
+		foreach (mysqli_query($link, "SELECT skater_num, name as skater_name FROM `registration` join skater on skater.num = skater_num where event_num='".$event."'") as $row) {
+			?>
+				<li>
+					<?= $row['skater_name'] ?>
+				</li>
+			<?php
+		}
+	?>
+</ul>
+<?php
 ?>
