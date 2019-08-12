10f1658a87d2316712bf5b53256ff0badaa7519e
Josh Donnoe <josh.donnoe@gmail.com>
2019-08-11 23:24:48 -0400

    Now register takes the get param instead of the included constant

--- register.php
+++ register.php
@@ -1,11 +1,10 @@
 <?php
 	require_once('db_connect.php');
-	require_once('event.php');
 ?>	
-<?= mysqli_query($link, "SELECT name as event_name from event where num=".$event) -> fetch_assoc()['event_name'] ?>
+<?= mysqli_query($link, "SELECT name as event_name from event where num=".$_GET['event_num']) -> fetch_assoc()['event_name'] ?>
 <ul>
 	<?php
-		foreach (mysqli_query($link, "SELECT skater_num, name as skater_name FROM `registration` join skater on skater.num = skater_num where event_num='".$event."'") as $row) {
+		foreach (mysqli_query($link, "SELECT skater_num, name as skater_name FROM `registration` join skater on skater.num = skater_num where event_num='".$_GET['event_num']."'") as $row) {
 			?>
 				<li>
 					<?= $row['skater_name'] ?>
