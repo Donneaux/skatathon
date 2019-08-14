06667088ac248616e169be471a3784a5c7088a77
Josh Donnoe <josh.donnoe@gmail.com>
2019-08-13 23:10:55 -0400

    record now records and queries for the count

--- record.php
+++ record.php
@@ -1,4 +1,7 @@
 <?php
 	require_once('db_connect.php');
-	mysqli_query($link, "INSERT INTO lap (event_num, skater_num) VALUES (".$_GET['event_num'].", ".$_GET['skater_num'].")")
+	mysqli_query($link, "INSERT INTO lap (event_num, skater_num) VALUES (".$_GET['event_num'].", ".$_GET['skater_num'].")");
+?>
+<?=
+	mysqli_query($link, "select count(*) as count from lap where event_num=".$_GET['event_num']) -> fetch_assoc()['count'];
 ?>
