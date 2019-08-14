c4953dd8f36d65b3d2cfefa88d52f577b439cf5a
Josh Donnoe <josh.donnoe@gmail.com>
2019-08-13 21:05:48 -0400

    track page shows event and skater and has  button

---
+++ track.php
@@ -0,0 +1,15 @@
+<?php
+	require_once('db_connect.php');
+	$result = mysqli_query($link, "select event.name as event_name, skater.name as skater_name from event,skater where (event.num, skater.num) = (".$_GET['event_num'].", ".$_GET['skater_num'].")") -> fetch_assoc();
+?>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("button").click(function(){
+			$.ajax("db_connect.php");
+		});
+	});
+</script>
+<?= $_GET['event_num'] ?><br /><?= $_GET['skater_num'] ?>
+<?= $result['event_name'] ?><br /><?= $result['skater_name'] ?>
+<button>LAP</button>
