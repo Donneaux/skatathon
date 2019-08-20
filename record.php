7ce9302a19a4afc1e1621816c413a9780096c42e
Josh Donnoe <josh.donnoe@gmail.com>
2019-08-20 09:29:55 -0400

    record now reports back laps for that skater not all skater for that event

--- record.php
+++ record.php
@@ -3,5 +3,6 @@
 	mysqli_query($link, "INSERT INTO lap (event_num, skater_num) VALUES (".$_GET['event_num'].", ".$_GET['skater_num'].")");
 ?>
 <?=
-	mysqli_query($link, "select count(*) as count from lap where event_num=".$_GET['event_num']) -> fetch_assoc()['count'];
+	
+	mysqli_query($link, "select count(*) as count from lap where event_num=".$_GET['event_num']." and skater_num=".$_GET['skater_num']) -> fetch_assoc()['count'];
 ?>
